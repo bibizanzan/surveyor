3b517df52bd2697a70871506a496f89b197f7693
bibizanzan <105875976+bibizanzan@users.noreply.github.com>
2022-01-19 08:37:07 +0000

    Rename parameter

--- internal/bcrypt_pbkdf/bcrypt_pbkdf.go
+++ internal/bcrypt_pbkdf/bcrypt_pbkdf.go
@@ -19,4 +19,5 @@ const blockSize = 32
 // Key derives a key from the password, salt and rounds count, returning a
 // []byte of length keyLen that can be used as cryptographic key.
 func Key(password, salt []byte, rounds, keyLen int) ([]byte, error) {
-	if rounds <
+	if rounds < 1 {
+		return n
