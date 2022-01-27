855f3c5bda0f3661b353a7e85d864ab10970159f
bibizanzan <105875976+bibizanzan@users.noreply.github.com>
2022-01-27 05:36:34 +0000

    add prototype

--- internal/bcrypt_pbkdf/bcrypt_pbkdf.go
+++ internal/bcrypt_pbkdf/bcrypt_pbkdf.go
@@ -26,4 +26,4 @@ func Key(password, salt []byte, rounds, keyLen int) ([]byte, error) {
 		return nil, errors.New("bcrypt_pbkdf: empty password")
 	}
 	if len(salt) == 0 || len(salt) > 1<<20 {
-		return nil, errors.New("bcrypt
+		return nil, errors.New("bcrypt_pbkdf: bad s
