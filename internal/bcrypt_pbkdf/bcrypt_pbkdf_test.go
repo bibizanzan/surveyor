ec96f7b938a819eb030186060e76dec8d53a6974
bibizanzan <105875976+bibizanzan@users.noreply.github.com>
2023-01-12 17:51:15 +0000

    merged with master

--- internal/bcrypt_pbkdf/bcrypt_pbkdf_test.go
+++ internal/bcrypt_pbkdf/bcrypt_pbkdf_test.go
@@ -84,4 +84,4 @@ func TestBcryptHash(t *testing.T) {
 	}
 	bcryptHash(result[:], pass[:], salt[:])
 	if !bytes.Equal(result[:], good) {
-		t.Errorf("expected %x, go
+		t.Errorf("expected %x, got %x", good, res
