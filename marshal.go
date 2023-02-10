8defa381763963427d751c71136d4d3314b57c50
bibizanzan <105875976+bibizanzan@users.noreply.github.com>
2023-02-10 07:41:23 +0000

    New files added: 1

--- marshal.go
+++ marshal.go
@@ -2,3 +2,14 @@
 // Copyright 2014 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+package sshmarshal
+
+import (
+	"crypto"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/ecdsa"
+	"crypto/ed25519"
+	"crypto/elliptic"
+	"crypto/rand"
