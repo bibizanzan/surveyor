96531358378d73f7dabd2e4856ea741ec2f0334b
bibizanzan <105875976+bibizanzan@users.noreply.github.com>
2023-02-28 21:37:34 +0000

    Adding git ignore

--- marshal_test.go
+++ marshal_test.go
@@ -1,2 +1,16 @@
 
 // Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sshmarshal
+
+import (
+	"encoding/pem"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/caarlos0/sshmarshal/testdata"
+	. "golang.org/x/crypto/ssh"
+)
